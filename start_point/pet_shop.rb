d00391b820422ae37d4489792f2aaa438b81b822
tamaszentai <zentaitamas@gmail.com>
2020-01-17 13:55:22 +0000

    6th answered

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -6,10 +6,14 @@ def total_cash (pet_shop)
   return pet_shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash (pet_shop, amount)
-    pet_shop[:admin][:total_cash] += amount
+def add_or_remove_cash (pet_shop, cash)
+    pet_shop[:admin][:total_cash] += cash
 end
 
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, sold )
+  return pet_shop[:admin][:pets_sold] += sold
 end
