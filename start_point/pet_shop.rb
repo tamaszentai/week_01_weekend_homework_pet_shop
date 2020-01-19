b53052ce36a0130c91114617f2eb2f4ab6f0e7e6
tamaszentai <zentaitamas@gmail.com>
2020-01-19 14:07:47 +0000

    full homework done

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -43,3 +43,76 @@ def find_pet_by_name(pet_shop, pet_name)
   end
   return nil
 end
+
+
+def remove_pet_by_name(pet_shop, pet_name)
+ for pet in pet_shop[:pets]
+   if pet_name == pet[:name]
+     pet_shop[:pets].delete(pet)
+   end
+  end
+end
+
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets].push(new_pet)
+  return pet_shop[:pets].count
+end
+
+def customer_cash (customer)
+return customer[:cash]
+end
+
+
+def remove_customer_cash(customer, amount)
+  customer[:cash] -= amount
+  return customer[:cash]
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count()
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+  return customer[:pets].count()
+end
+
+#############OPTIONAL###############
+
+def customer_can_afford_pet (customer, new_pet)
+  if customer[:cash] >= new_pet[:price]
+    return true
+  else
+    return false
+  end
+end
+
+
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  add_pet_to_customer(customer, pet)
+  increase_pets_sold(pet_shop, 1)
+  remove_customer_cash(customer, pet[:price])
+  add_or_remove_cash(pet_shop, pet[:price])
+end
+
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  if pet != nil
+    add_pet_to_customer(customer, pet)
+    increase_pets_sold(pet_shop, 1)
+    remove_customer_cash(customer, pet[:price])
+    add_or_remove_cash(pet_shop, pet[:price])
+  else
+  end
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  if pet != nil && customer_can_afford_pet(customer, pet) == true
+    add_pet_to_customer(customer, pet)
+    increase_pets_sold(pet_shop, 1)
+    remove_customer_cash(customer, pet[:price])
+    add_or_remove_cash(pet_shop, pet[:price])
+  end
+end
